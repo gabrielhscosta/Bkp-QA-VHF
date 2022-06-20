fc8c2b258d7a3ab3a62629fc63eba02eece331a9
jose.xavier <jose.xavier@d0007azurecloudtotvscom.onmicrosoft.com>
2022-06-20 10:36:07 -0300

    Inserção do gerador de hospede fake

---
+++ VHF/CommonMethods/GeradorDadosFakes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using Bogus.Extensions.Brazil;
+
+namespace VHF.CommonMethods
+{
+    public class GeradorDadosFakes
+    {
+
+        public static HospedeFake ListaDadosFakerPessoa()
+        {
+            var dadosFaker = new Faker<HospedeFake>("pt_BR").StrictMode(true)
+                .RuleFor(p => p.CPFFaker, f => f.Person.Cpf())
+                .RuleFor(p => p.NomeFaker, f => f.Name.FirstName(Bogus.DataSets.Name.Gender.Female))
+                .RuleFor(p => p.SobrenomeFaker, f => f.Name.LastName(Bogus.DataSets.Name.Gender.Female))
+                .RuleFor(p => p.EmailFaker, f => f.Internet.Email(f.Person.FirstName, f.Person.LastName).ToLower())
+                .RuleFor(p => p.DtNascFaker, f => "24031989")
+                .RuleFor(p => p.CEPFaker, f => "57062422")
+                .RuleFor(p => p.CidadeFaker, f => "Maceio");
+            var pessoa = dadosFaker.Generate();
+
+            return pessoa;
+
+        }
+
+    }
+}
---
+++ VHF/CommonMethods/HospedeFake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHF.CommonMethods
+{
+    public class HospedeFake
+    {
+        public string CPFFaker { get; set; }
+        public string NomeFaker { get; set; }
+        public string SobrenomeFaker { get; set; }
+        public string EmailFaker { get; set; }
+        public string DtNascFaker { get; set; }
+        public string CEPFaker { get; set; }
+        public string CidadeFaker { get; set; }
+    }
+}
